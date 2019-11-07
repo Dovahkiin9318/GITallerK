bb5739b256d5efd4797869e465c2dae5c098a2a0
mmm <joijoi>
2019-11-07 16:44:02 -0600

    Primer commit

---
+++ GITallerK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GITallerK
+//
+//  Created by CEDAM09 on 07/11/19.
+//  Copyright © 2019 CEDAM09. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
